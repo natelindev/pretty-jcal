bfc5317c49ac288fdbb04bf59019b841ed876ba9
Nathaniel <linlilulll@gmail.com>
2020-12-31 15:06:16 +0800

    fixed undefined

--- src/jcal2prettyJCAL.ts
+++ src/jcal2prettyJCAL.ts
@@ -4,12 +4,12 @@ export const jcal2prettyJCAL = (jcal: JCALComponentNode): PrettyJCALComponentNod
   const [key, props, components] = jcal;
   return {
     name: key,
-    properties: props.map((p) => ({
+    properties: props? props.map((p) => ({
       name: p[0],
       parameters: Object.keys(p[1]).length > 0 ? p[1] : undefined,
       type: p[2],
       value: p[3],
-    })),
-    components: components.map((component: JCALComponentNode) => jcal2prettyJCAL(component)),
+    })): [],
+    components: components?  components.map((component: JCALComponentNode) => jcal2prettyJCAL(component)): [],
   };
 };
